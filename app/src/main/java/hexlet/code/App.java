8053ce4b338628a0f822bf21d7c7b733f5377ae0
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-11-02 19:40:43 +0500

    feat: create Javalin app in main class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,23 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        getApp().start(7070);
     }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return app;
+    }
+
+
 }
