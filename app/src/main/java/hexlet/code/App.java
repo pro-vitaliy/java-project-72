79bbde05f648eec06c9a0dc5d0000821ba884a8b
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-11-03 22:34:38 +0500

    feat: Add method to initialize database connection

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,15 +1,41 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "7070"));
         getApp().start(port);
     }
 
-    public static Javalin getApp() {
+    public static void initializeDataSource() throws Exception {
+        var hikariConfig = new HikariConfig();
+        var dbUrlDefault = "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;";
+        var dbUrl = System.getenv().getOrDefault("JDBC_DATABASE_URL", dbUrlDefault);
+        hikariConfig.setJdbcUrl(dbUrl);
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        var url = App.class.getClassLoader().getResourceAsStream("schema.sql");
+        var sql = new BufferedReader(new InputStreamReader(url))
+                .lines().collect(Collectors.joining("\n"));
+        try (var conn = dataSource.getConnection();
+                var statement = conn.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+    }
+
+    public static Javalin getApp() throws Exception {
+        initializeDataSource();
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
