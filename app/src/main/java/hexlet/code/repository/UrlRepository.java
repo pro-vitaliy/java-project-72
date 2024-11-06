a2a101cdf5c7f3214e9cc78b329b5ba5a497561c
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-11-06 23:09:26 +0500

    feat: add getEntities method to UrlRepository. Update save method

--- app/src/main/java/hexlet/code/repository/UrlRepository.java
+++ app/src/main/java/hexlet/code/repository/UrlRepository.java
@@ -3,19 +3,46 @@ package hexlet.code.repository;
 import hexlet.code.model.Url;
 
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UrlRepository extends BaseRepository {
     public static void save(Url url) throws SQLException {
-        String sql = "INSERT INTO urls (name, created_at) VALUES (?, CURRENT_TIMESTAMP) RETURNING id, created_at;";
+        var sql = "INSERT INTO urls (name, created_at) VALUES (?, ?)";
+        try (var connection = dataSource.getConnection();
+                var statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, url.getName());
+            var timeStamp = Timestamp.valueOf(LocalDateTime.now());
+            statement.setTimestamp(2, timeStamp);
+            statement.executeUpdate();
+            var generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                url.setId(generatedKeys.getLong("id"));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
+            }
+        }
+    }
+
+    public static List<Url> getEntities() throws SQLException {
+        var sql = "SELECT * FROM urls";
         try (var connection = dataSource.getConnection();
                 var statement = connection.prepareStatement(sql)) {
-            statement.setString(1, url.getName());
-            try (var resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    url.setId(resultSet.getLong("id"));
-                    url.setCreatedAt(resultSet.getTimestamp("created_at"));
-                }
+            var resultSet = statement.executeQuery();
+            var result = new ArrayList<Url>();
+            while (resultSet.next()) {
+                var id = resultSet.getLong("id");
+                var name = resultSet.getString("name");
+                var createdAt = resultSet.getTimestamp("created_at");
+                var url = new Url(name);
+                url.setId(id);
+                url.setCreatedAt(createdAt);
+                result.add(url);
             }
+            return result;
         }
     }
 }
