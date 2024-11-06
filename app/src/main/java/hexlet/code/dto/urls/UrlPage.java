6715401bf27feebc5fdd9c3a4f0db4706f8f8028
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-11-06 23:20:36 +0500

    feat: add dto class UrlPage for Url entity

---
+++ app/src/main/java/hexlet/code/dto/urls/UrlPage.java
@@ -0,0 +1,11 @@
+package hexlet.code.dto.urls;
+
+import hexlet.code.model.Url;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+public class UrlPage {
+    private Url url;
+}
