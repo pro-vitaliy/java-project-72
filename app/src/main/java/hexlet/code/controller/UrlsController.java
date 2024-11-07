2a7a7b179f0d125fd44cad92d8afd596443bd7a2
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-11-08 00:41:19 +0500

    refactor: update save method add check url contains in DB

--- app/src/main/java/hexlet/code/controller/UrlsController.java
+++ app/src/main/java/hexlet/code/controller/UrlsController.java
@@ -1,9 +1,12 @@
 package hexlet.code.controller;
 
+import hexlet.code.dto.BasePage;
+import hexlet.code.dto.urls.UrlPage;
 import hexlet.code.dto.urls.UrlsPage;
 import hexlet.code.model.Url;
 import hexlet.code.repository.UrlRepository;
 import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
 
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -27,21 +30,34 @@ public class UrlsController {
             if (port != -1) {
                 parsedUrl = parsedUrl + ":" + port;
             }
-            var url = new Url(parsedUrl);
-            UrlRepository.save(url);
-            ctx.redirect("/urls");
-        } catch (URISyntaxException | MalformedURLException e) {
-            ctx.render("index.jte");
+            if (UrlRepository.urlContains(parsedUrl)) {
+                ctx.sessionAttribute("flash", "Страница уже существует");
+                ctx.redirect("/urls");
+            } else {
+                var url = new Url(parsedUrl);
+                UrlRepository.save(url);
+                ctx.sessionAttribute("flash", "Страница успешно добавлена");
+                ctx.redirect("/urls");
+            }
+        } catch (URISyntaxException | MalformedURLException | IllegalArgumentException e) {
+            ctx.sessionAttribute("flash", "Некорректный URL");
+            var page = new BasePage();
+            page.setFlash(ctx.consumeSessionAttribute("flash"));
+            ctx.render("index.jte", model("page", page));
         }
     }
 
     public static void index(Context ctx) throws SQLException {
         var page = new UrlsPage(UrlRepository.getEntities());
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
         ctx.render("urls/index.jte", model("page", page));
     }
 
-    public static void show(Context ctx) {
+    public static void show(Context ctx) throws SQLException {
         var id = ctx.pathParamAsClass("id", Long.class).get();
-//        UrlRepository.find(id);
+        var url = UrlRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Url with id=" + id + "not found"));
+        var page = new UrlPage(url);
+        ctx.render("urls/show.jte", model("page", page));
     }
 }
