dfa6fff29b3bcae6cdc72549fa554b7a71c325cd
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-11-15 17:30:29 +0500

    fix: handle missing schema.sql file

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,6 +12,7 @@ import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
@@ -30,6 +31,9 @@ public class App {
 
         var dataSource = new HikariDataSource(hikariConfig);
         var url = App.class.getClassLoader().getResourceAsStream("schema.sql");
+        if (url == null) {
+            throw new FileNotFoundException("schema.sql not found");
+        }
         var sql = new BufferedReader(new InputStreamReader(url))
                 .lines().collect(Collectors.joining("\n"));
         try (var conn = dataSource.getConnection();
