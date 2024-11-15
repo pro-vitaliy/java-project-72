484ea58f545f7d84c2d7a88e65c5a6105656e75a
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-11-15 17:08:40 +0500

    refactor: update save method to assign generated ID to urlCheck, rename find method

--- app/src/main/java/hexlet/code/repository/UrlCheckRepository.java
+++ app/src/main/java/hexlet/code/repository/UrlCheckRepository.java
@@ -3,6 +3,7 @@ package hexlet.code.repository;
 import hexlet.code.model.UrlCheck;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -17,7 +18,7 @@ public class UrlCheckRepository extends BaseRepository {
                 """;
 
         try (var connection = dataSource.getConnection();
-                var statement = connection.prepareStatement(sql)) {
+                var statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             statement.setLong(1, check.getUrlId());
             statement.setInt(2, check.getStatusCode());
             statement.setString(3, check.getH1());
@@ -26,10 +27,16 @@ public class UrlCheckRepository extends BaseRepository {
             var timeStamp = Timestamp.valueOf(LocalDateTime.now());
             statement.setTimestamp(6, timeStamp);
             statement.executeUpdate();
+            var generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                check.setId(generatedKeys.getLong(1));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
+            }
         }
     }
 
-    public static List<UrlCheck> find(Long urlId) throws SQLException {
+    public static List<UrlCheck> findByUrlId(Long urlId) throws SQLException {
         var sql = "SELECT * FROM urls_check WHERE url_id = ?";
 
         try (var connection = dataSource.getConnection();
