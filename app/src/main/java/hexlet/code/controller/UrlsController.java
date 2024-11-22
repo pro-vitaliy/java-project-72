c9c82610278ffe0cffef2783a55a20555848cad1
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-11-23 03:10:36 +0500

    refactor: switched List to Map in DTO class UrlsPage, updated usages

--- app/src/main/java/hexlet/code/controller/UrlsController.java
+++ app/src/main/java/hexlet/code/controller/UrlsController.java
@@ -17,6 +17,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
 
 import org.jsoup.Jsoup;
 
@@ -55,7 +56,14 @@ public class UrlsController {
     }
 
     public static void index(Context ctx) throws SQLException {
-        var page = new UrlsPage(UrlRepository.getEntities());
+        var urls = UrlRepository.getEntities();
+        var urlsInfo = new LinkedHashMap<Url, UrlCheck>();
+        for (var url : urls) {
+            var lastCheck = UrlCheckRepository.findLastCheck(url.getId())
+                    .orElse(new UrlCheck(url.getId()));
+            urlsInfo.put(url, lastCheck);
+        }
+        var page = new UrlsPage(urlsInfo);
         page.setFlash(ctx.consumeSessionAttribute("flash"));
         ctx.render("urls/index.jte", model("page", page));
     }
--- app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
@@ -3,12 +3,14 @@ package hexlet.code.dto.urls;
 import hexlet.code.dto.BasePage;
 import hexlet.code.model.Url;
 
-import java.util.List;
+import java.util.Map;
+
+import hexlet.code.model.UrlCheck;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Getter
 public class UrlsPage extends BasePage {
-    private List<Url> urls;
+    private Map<Url, UrlCheck> urls;
 }
